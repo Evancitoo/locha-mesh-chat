8b750538e2ebd2f87d8554ebf2e168972c225248
xkevin190 <kevinvelasco193@gmail.com>
2020-07-15 20:09:54 -0400

    solved problems the webtorren library in compilation, there are still dns errors library

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ import App from './App';
 import { name as appName } from './app.json';
 import './shim';
 
+const DHT = require('bittorrent-dht');
+
+ const dht = new DHT();
+
+// dht.listen(20000, () => {
+//   console.log('now listening');
+// });
+
+// dht.on('peer', (peer, infoHash, from) => {
+//   console.log(`found potential peer ${peer.host}:${peer.port} through ${from.address}:${from.port}`);
+// });
+
+// find peers for the given torrent info hash
+// dht.lookup(parsed.infoHash);
+
+
 AppRegistry.registerComponent(appName, () => App);
