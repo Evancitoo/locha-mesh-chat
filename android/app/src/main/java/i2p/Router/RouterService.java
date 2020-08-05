8410d8feb04f083465c5a507aa4da35650ea109a
xkevin190 <kevinvelasco193@gmail.com>
2020-08-04 22:15:37 -0400

    running router with tunel i2p

--- android/app/src/main/java/i2p/Router/RouterService.java
+++ android/app/src/main/java/i2p/Router/RouterService.java
@@ -10,6 +10,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
@@ -19,12 +20,39 @@ import androidx.core.app.NotificationCompat;
 
 import com.lochameshchat.R;
 
+import net.i2p.data.DataHelper;
+import net.i2p.router.Job;
 import net.i2p.router.Router;
 import net.i2p.router.RouterContext;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import i2p.I2pModule;
+import i2p.Util.Notifications;
+import i2p.Util.Util;
 
 public class RouterService extends Service {
+
+    /**
+     * A request to this service for the current router state. Broadcasting
+     * this will trigger a state notification.
+     */
+    public static final String LOCAL_BROADCAST_REQUEST_STATE = "net.i2p.android.LOCAL_BROADCAST_REQUEST_STATE";
+    /**
+     * A notification of the current state. This is informational; the state
+     * has not changed.
+     */
+    public static final String LOCAL_BROADCAST_STATE_NOTIFICATION = "net.i2p.android.LOCAL_BROADCAST_STATE_NOTIFICATION";
+    /**
+     * The state has just changed.
+     */
+    public static final String LOCAL_BROADCAST_STATE_CHANGED = "net.i2p.android.LOCAL_BROADCAST_STATE_CHANGED";
+    public static final String LOCAL_BROADCAST_EXTRA_STATE = "net.i2p.android.STATE";
+    /**
+     * The locale has just changed.
+     */
+    public static final String LOCAL_BROADCAST_LOCALE_CHANGED = "net.i2p.android.LOCAL_BROADCAST_LOCALE_CHANGED";
 
 
     private final static  String TAG = "RouterService";
@@ -36,11 +64,17 @@ public class RouterService extends Service {
     private static final String EXTRA_RESTART = "restar";
     private Thread starterThread;
     private RouterContext routerContext;
+    private Notifications _notif;
+    private Handler handler;
+    private Updater updater;
+    private int status = 0;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        _notif = new Notifications(this);
+        handler = new Handler();
+        updater = new Updater();
         if (Build.VERSION.SDK_INT >= 26) {
             String CHANNEL_ID = "my_channel_01";
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
@@ -82,6 +116,9 @@ public class RouterService extends Service {
             starterThread = new Thread( new Starter());
             starterThread.start();
         }
+
+        handler.removeCallbacks(updater);
+        handler.postDelayed(updater, 50);
         return START_STICKY;
     }
 
@@ -106,18 +143,75 @@ public class RouterService extends Service {
                     throw new IllegalStateException("Router has no context?");
                 }
 
+                status = 1;
                 routerContext.router().setKillVMOnEnd(false);
-
-
+                Job loadJob = new LoadClientsJob(RouterService.this, routerContext, _notif);
+                routerContext.jobQueue().addJob(loadJob);
+                starterThread = null;
             }
             Log.d(TAG ," running router");
+            updateStatus(routerContext);
+        }
+    }
 
+
+    private class Updater implements Runnable {
+
+        public void run() {
+            RouterContext ctx = routerContext;
+            Log.i(TAG, "status"+ status );
+            if(status != 0){
+                Router router = ctx.router();
+                if(router.isAlive()) {
+                    updateStatus(ctx);
+                }
+                handler.postDelayed(this, 15 * 1000);
+            }
         }
     }
 
 
 
+    private void updateStatus(RouterContext ctx) {
 
+
+        int active = ctx.commSystem().countActivePeers();
+        int known = Math.max(ctx.netDb().getKnownRouters() - 1, 0);
+        int inEx = ctx.tunnelManager().getFreeTunnelCount();
+        int outEx = ctx.tunnelManager().getOutboundTunnelCount();
+        int inCl = ctx.tunnelManager().getInboundClientTunnelCount();
+        int outCl = ctx.tunnelManager().getOutboundClientTunnelCount();
+        String uptime = DataHelper.formatDuration(ctx.router().getUptime());
+        double inBW = ctx.bandwidthLimiter().getReceiveBps();
+        double outBW = ctx.bandwidthLimiter().getSendBps();
+
+        String text =
+                getResources().getString(R.string.notification_status_text,
+                        Util.formatSpeed(inBW), Util.formatSpeed(outBW));
+
+        String bigText =
+                getResources().getString(R.string.notification_status_bw,
+                        Util.formatSpeed(inBW), Util.formatSpeed(outBW)) + '\n'
+                        + getResources().getString(R.string.notification_status_peers,
+                        active, known) + '\n'
+                        + getResources().getString(R.string.notification_status_expl,
+                        inEx, outEx) + '\n'
+                        + getResources().getString(R.string.notification_status_client,
+                        inCl, outCl);
+
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                Log.i(TAG, "text one" + text);
+                Log.i(TAG, "text two" + bigText);
+            }
+        }, 10000, 5000);
+
+
+    }
 
     @Override
     public void onDestroy() {
--- android/app/src/main/java/i2p/Util/Util.java
+++ android/app/src/main/java/i2p/Util/Util.java
@@ -2,6 +2,7 @@ package i2p.Util;
 
 import net.i2p.router.RouterContext;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class Util {
@@ -18,5 +19,56 @@ public class Util {
             return contexts.get(0);
         }
         return null;
+    }
+
+
+    public static String formatSize(double size) {
+        return formatSize(size, 0);
+    }
+
+    public static String formatSpeed(double size) {
+        return formatSize(size, 1);
+    }
+
+    public static String formatSize(double size, int baseScale) {
+        int scale;
+        for (int i = 0; i < baseScale; i++) {
+            size /= 1024.0D;
+        }
+        for (scale = baseScale; size >= 1024.0D; size /= 1024.0D) {
+            ++scale;
+        }
+
+        // control total width
+        DecimalFormat fmt;
+        if (size >= 1000) {
+            fmt = new DecimalFormat("#0");
+        } else if (size >= 100) {
+            fmt = new DecimalFormat("#0.0");
+        } else {
+            fmt = new DecimalFormat("#0.00");
+        }
+
+        String str = fmt.format(size);
+        switch (scale) {
+            case 1:
+                return str + "K";
+            case 2:
+                return str + "M";
+            case 3:
+                return str + "G";
+            case 4:
+                return str + "T";
+            case 5:
+                return str + "P";
+            case 6:
+                return str + "E";
+            case 7:
+                return str + "Z";
+            case 8:
+                return str + "Y";
+            default:
+                return str + "";
+        }
     }
 }
